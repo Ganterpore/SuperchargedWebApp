afe595760d276edca69e91e4031bb8a7d023bc13
Mitchell Hatfield <mitchell.hatfield@ue.com.au>
2023-01-30 19:16:25 +1100

    Converted into a PWA

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,12 @@ function App() {
       <header className="App-header">
         Supercharged Training
       </header>
-      <body className="App-body">
+      <div className="App-body">
         <img src={symbolOrangeWhite} className="App-logo" alt="logo" />
         <Quote/>
         <div className="App-spacer"></div>
         <button className="App-button Workout-button" onClick={() => console.log('Do Something')}>Start Workout</button>
-      </body>
+      </div>
     </div>
   );
 }
